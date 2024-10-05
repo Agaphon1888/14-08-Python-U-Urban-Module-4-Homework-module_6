7e528ed7c122c371ba7b200824f3414404fb6d20
Agaphon1888 <egorov2000@inbox.ru>
2024-10-05 21:54:50 +0500

    Домашнее задание по теме "Множественное наследование". Задача "Мифическое наследование".

---
+++ module_6_3.py
@@ -0,0 +1,48 @@
+# Домашнее задание по теме "Множественное наследование".
+# Задача "Мифическое наследование".
+
+class Horse:
+    x_distance = 0
+    sound = 'Frrr'
+
+    def run(self, dx):
+        self.x_distance += dx
+        return self.x_distance
+
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy):
+        self.y_distance += dy
+        return self.y_distance
+
+
+class Pegasus(Horse, Eagle):
+    sound = 'I train, eat, sleep, and repeat'
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        tpl = (self.x_distance, self.y_distance)
+        return tpl
+
+    def voice(self):
+        print(f'{self.sound}')
+
+
+def main():
+    p1 = Pegasus()
+    print(p1.get_pos())
+    p1.move(10, 15)
+    print(p1.get_pos())
+    p1.move(-5, 20)
+    print(p1.get_pos())
+    p1.voice()
+
+
+if __name__ == '__main__':
+    main()
