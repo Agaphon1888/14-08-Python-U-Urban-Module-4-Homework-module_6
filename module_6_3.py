03c53033797a3bd9c66538d7c5ff0f4b622f55ae
Agaphon1888 <egorov2000@inbox.ru>
2024-10-07 15:13:48 +0500

    Доработал согласно замечанию.

--- module_6_3.py
+++ module_6_3.py
@@ -2,46 +2,47 @@
 # Задача "Мифическое наследование".
 
 class Horse:
-    x_distance = 0
-    sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx):
         self.x_distance += dx
-        return self.x_distance
 
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
-        return self.y_distance
 
 
 class Pegasus(Horse, Eagle):
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        self.run(dx)  # вызываем метод из класса Horse
+        self.fly(dy)  # вызываем метод из класса Eagle
 
     def get_pos(self):
-        tpl = (self.x_distance, self.y_distance)
-        return tpl
+        return self.x_distance, self.y_distance
 
     def voice(self):
-        print(f'{self.sound}')
+        print(self.sound)  # выводим звук из атрибута экземпляра
 
 
 def main():
     p1 = Pegasus()
-    print(p1.get_pos())
+    print(p1.get_pos())  # Начальная позиция (0,0)
     p1.move(10, 15)
-    print(p1.get_pos())
+    print(p1.get_pos())  # Позиция после первого движения (10, 15)
     p1.move(-5, 20)
-    print(p1.get_pos())
-    p1.voice()
+    print(p1.get_pos())  # Позиция после второго движения (5, 35)
+    p1.voice()  # Вывод звука Пегаса
 
 
 if __name__ == '__main__':
